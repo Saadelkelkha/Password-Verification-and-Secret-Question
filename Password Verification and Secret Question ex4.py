17e98f4c4288b694ec5c10e5401db2795958e564
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2023-10-18 23:01:08 +0100

    Add files via upload

---
+++ Password Verification and Secret Question ex4.py
@@ -0,0 +1,10 @@
+for i in range( 0,3) :  
+    mot=input("Type password")
+    if mot=="DEV@2023" :
+        print("Hello")
+if mot!="DEV@2023" :
+    j=input("Type the name of the favorite film")
+    if j=="Game of Thrones" :
+        print("Hello")
+    else :
+        print ("False")
